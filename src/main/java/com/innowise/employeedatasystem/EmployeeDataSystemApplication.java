22d42d0c22dc19c6681790d1e070c6293fa97088
Lelikbjmak <denissolovey990@gmail.com>
2023-03-15 20:49:31 +0300

    Initial commit

---
+++ src/main/java/com/innowise/employeedatasystem/EmployeeDataSystemApplication.java
@@ -0,0 +1,12 @@
+package com.innowise.employeedatasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDataSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeDataSystemApplication.class);
+    }
+}
