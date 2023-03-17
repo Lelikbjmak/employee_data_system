db0df5335a70d52cf06f26185e00429429bf210e
Lelikbjmak <denissolovey990@gmail.com>
2023-03-18 00:21:11 +0300

    feat: Add RoleIsNotFoundException

---
+++ src/main/java/com/innowise/employeedatasystem/exception/RoleIsNotFoundException.java
@@ -0,0 +1,8 @@
+package com.innowise.employeedatasystem.exception;
+
+public class RoleIsNotFoundException extends RuntimeException {
+
+    public RoleIsNotFoundException(String message) {
+        super(message);
+    }
+}
