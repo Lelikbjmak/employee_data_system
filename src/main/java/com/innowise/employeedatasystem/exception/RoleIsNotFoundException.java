4081783f98bfcab8986375e069565c86a50ab7d2
Lelikbjmak <denissolovey990@gmail.com>
2023-03-19 23:26:05 +0300

    feat: Update RoleIsNotFoundException

--- src/main/java/com/innowise/employeedatasystem/exception/RoleIsNotFoundException.java
+++ src/main/java/com/innowise/employeedatasystem/exception/RoleIsNotFoundException.java
@@ -1,8 +1,21 @@
 package com.innowise.employeedatasystem.exception;
 
-public class RoleIsNotFoundException extends RuntimeException {
+import lombok.Getter;
 
-    public RoleIsNotFoundException(String message) {
+import java.time.Instant;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@Getter
+public class RoleIsNotFoundException extends NoSuchElementException {
+
+    private Instant timestamp;
+
+    private Map<String, Object> additional;
+
+    public RoleIsNotFoundException(String message, Instant instant, Map<String, Object> additional) {
         super(message);
+        this.timestamp = instant;
+        this.additional = additional;
     }
 }
