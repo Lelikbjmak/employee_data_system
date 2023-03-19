539e1d74bb9e9c41ee379694e98833653ff95979
Lelikbjmak <denissolovey990@gmail.com>
2023-03-19 23:27:46 +0300

    fix: Update return type of query to find Role. Was: Optional<Role>, become: Role.

--- src/main/java/com/innowise/employeedatasystem/repo/RoleRepository.java
+++ src/main/java/com/innowise/employeedatasystem/repo/RoleRepository.java
@@ -4,10 +4,8 @@ import com.innowise.employeedatasystem.entity.Role;
 import com.innowise.employeedatasystem.entity.RoleEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    Optional<Role> findByRole(RoleEnum role);
+    Role findByRole(RoleEnum role);
 
 }
