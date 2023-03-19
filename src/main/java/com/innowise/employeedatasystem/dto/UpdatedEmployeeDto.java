46c980df4358685ee4e7506efcf5a5b27c046dd8
Lelikbjmak <denissolovey990@gmail.com>
2023-03-19 23:29:02 +0300

    feat: Add UpdatedEmployeeDto to observe just edited Employee

---
+++ src/main/java/com/innowise/employeedatasystem/dto/UpdatedEmployeeDto.java
@@ -0,0 +1,25 @@
+package com.innowise.employeedatasystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdatedEmployeeDto {
+
+    private String firstName;
+
+    private String middleName;
+
+    private String lastName;
+
+    private Date hireDate;
+
+    private boolean isUpdated;
+}
