4160d0f359067c978a41a5432a0e10a2494721d3
Lelikbjmak <denissolovey990@gmail.com>
2023-03-19 23:31:51 +0300

    feat: Add edit(Edit existing Employee (for admin)), delete (delete existing Employee (for admin)), get(get existing Employee) mappings.

--- src/main/java/com/innowise/employeedatasystem/controller/EmployeeController.java
+++ src/main/java/com/innowise/employeedatasystem/controller/EmployeeController.java
@@ -1,8 +1,7 @@
 package com.innowise.employeedatasystem.controller;
 
-import com.innowise.employeedatasystem.dto.EmployeeDto;
-import com.innowise.employeedatasystem.dto.RegistrationDto;
-import com.innowise.employeedatasystem.dto.RegistrationResponseDto;
+import com.innowise.employeedatasystem.dto.*;
+import com.innowise.employeedatasystem.exception.EmployeeIsNotFoundException;
 import com.innowise.employeedatasystem.serviceimpl.EmployeeManagementServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,15 +19,41 @@ public class EmployeeController {
 
     @PostMapping("add")
     @PreAuthorize("hasRole('ADMIN')")
-    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ResponseStatus(HttpStatus.CREATED)
     public RegistrationResponseDto addEmployee(@RequestBody List<RegistrationDto> registrationDto) {
         return employeeManagementService.registerEmployees(registrationDto);
     }
 
+    @PostMapping("edit")
+    @PreAuthorize("hasRole('ADMIN')")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public List<UpdatedEmployeeDto> editEmployee(@RequestBody List<EmployeeDto> editEmployeeDtoList) {
+        return employeeManagementService.editEmployees(editEmployeeDtoList);
+    }
+
+    @PostMapping("delete")
+    @PreAuthorize("hasRole('ADMIN')")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public List<DeletedEmployeeDto> deleteEmployees(@RequestBody List<EmployeeDto> deleteEmployeeDtoList) {
+        return employeeManagementService.deleteEmployees(deleteEmployeeDtoList);
+    }
+
     @GetMapping("all")
+    @ResponseStatus(HttpStatus.OK)
     public List<EmployeeDto> getAll() {
         return employeeManagementService.getAllEmployees();
     }
 
+    @GetMapping("get/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public EmployeeDto getEmployeeById(@PathVariable(name = "id") Long id) throws EmployeeIsNotFoundException {
+        return employeeManagementService.getEmployeeById(id);
+    }
+
+    @GetMapping("get/by/user")
+    @ResponseStatus(HttpStatus.OK)
+    public EmployeeDto getEmployeeByUserUsername(@RequestParam(name = "username") String username) {
+        return employeeManagementService.getEmployeeByUserUsername(username);
+    }
 
 }
