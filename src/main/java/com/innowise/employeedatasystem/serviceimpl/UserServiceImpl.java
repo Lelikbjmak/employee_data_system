08306fa9fb166a6450ca9e169cb3c873e3711f5c
Lelikbjmak <denissolovey990@gmail.com>
2023-03-19 23:52:44 +0300

    fix: Change exception to `UserIsNotFoundException` if User wasn't found.

--- src/main/java/com/innowise/employeedatasystem/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/innowise/employeedatasystem/serviceimpl/UserServiceImpl.java
@@ -1,16 +1,21 @@
 package com.innowise.employeedatasystem.serviceimpl;
 
 import com.innowise.employeedatasystem.entity.User;
+import com.innowise.employeedatasystem.exception.UserIsNotFoundException;
 import com.innowise.employeedatasystem.repo.UserRepository;
 import com.innowise.employeedatasystem.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private static final String USERNAME_IS_NOT_FOUND_EXCEPTION_MESSAGE = "User is not found";
 
     private final UserRepository userRepository;
 
@@ -18,7 +23,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User findByUsername(String username) {
-        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("sad"));
+        return userRepository.findByUsername(username).orElseThrow(() ->
+                new UserIsNotFoundException(USERNAME_IS_NOT_FOUND_EXCEPTION_MESSAGE, Instant.now(), Map.of("Username", username)));
     }
 
     @Override
