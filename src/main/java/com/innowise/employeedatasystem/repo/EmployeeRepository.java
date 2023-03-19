5486b3fe2bc4f4ba9820b2a5114178412d459db7
Lelikbjmak <denissolovey990@gmail.com>
2023-03-19 23:50:09 +0300

    feat: Add find Employee method by several attributes.

--- src/main/java/com/innowise/employeedatasystem/repo/EmployeeRepository.java
+++ src/main/java/com/innowise/employeedatasystem/repo/EmployeeRepository.java
@@ -3,6 +3,10 @@ package com.innowise.employeedatasystem.repo;
 import com.innowise.employeedatasystem.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+    Optional<Employee> findByFirstNameAndLastNameAndMiddleNameAndHireDate(String firstName, String lastName, String middleName, Date hireDate);
 }
