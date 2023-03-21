c85ea4a4460c43bb7f33881f019cc2759d5bfc7c
Lelikbjmak <denissolovey990@gmail.com>
2023-03-21 23:37:32 +0300

    feat: Add sql scripts for testing.

---
+++ src/test/resources/sql/create-disabled-user-before-authentication.sql
@@ -0,0 +1,21 @@
+insert into roles values
+(1, 'ROLE_ADMIN'),
+(2, 'ROLE_USER');
+
+insert into employees values
+(1, 'Test',  NOW(), 'Tester', 'Testerov');
+
+insert into users values
+(1,
+true, -- account non expired
+true, -- account non locked
+true, -- credentials non expired
+false, -- enabled
+'test@gmail.com',
+'$argon2id$v=19$m=23552,t=2,p=1$xnr/bs027Dlc/fVMbrapcRU$R5NvPkutHQfnx5fG6FTt3iaUMIu0i/5x8CMpiNekgGs',
+'test');
+
+insert into users_roles values
+(1, 1);
+
+
---
+++ src/test/resources/sql/create-expired-user-before-authentication.sql
@@ -0,0 +1,19 @@
+insert into roles values
+(1, 'ROLE_ADMIN'),
+(2, 'ROLE_USER');
+
+insert into employees values
+(1, 'Test',  NOW(), 'Tester', 'Testerov');
+
+insert into users values
+(1,
+false, -- account non expired
+true, -- account non locked
+false, -- credentials non expired
+true, -- enabled
+'test@gmail.com',
+'$argon2id$v=19$m=23552,t=2,p=1$xnr/bs027Dlc/fVMbrapcRU$R5NvPkutHQfnx5fG6FTt3iaUMIu0i/5x8CMpiNekgGs',
+'test');
+
+insert into users_roles values
+(1, 1);
