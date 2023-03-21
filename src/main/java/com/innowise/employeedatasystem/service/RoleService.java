fdee39dbae60ac6d684c72ba7282ca42f796a849
Lelikbjmak <denissolovey990@gmail.com>
2023-03-21 23:41:53 +0300

    feat: Add exception if Role was not found in Database in findByRole() method.

--- src/main/java/com/innowise/employeedatasystem/service/RoleService.java
+++ src/main/java/com/innowise/employeedatasystem/service/RoleService.java
@@ -3,8 +3,10 @@ package com.innowise.employeedatasystem.service;
 import com.innowise.employeedatasystem.entity.Role;
 import com.innowise.employeedatasystem.entity.RoleEnum;
 
+import javax.management.relation.RoleInfoNotFoundException;
+
 public interface RoleService {
 
-    Role findByRole(RoleEnum role);
+    Role findByRole(RoleEnum role) throws RoleInfoNotFoundException;
 
 }
