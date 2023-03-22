87f056477b4f09e105e5af5ed351ad95c7aa6622
Lelikbjmak <denissolovey990@gmail.com>
2023-03-22 22:07:30 +0300

    feat: Add RoleServiceTest

---
+++ src/test/java/com/innowise/employeedatasystem/service/RoleServiceTest.java
@@ -0,0 +1,71 @@
+package com.innowise.employeedatasystem.service;
+
+import com.innowise.employeedatasystem.entity.Role;
+import com.innowise.employeedatasystem.entity.RoleEnum;
+import com.innowise.employeedatasystem.exception.RoleIsNotFoundException;
+import com.innowise.employeedatasystem.repo.RoleRepository;
+import com.innowise.employeedatasystem.serviceimpl.RoleServiceImpl;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class RoleServiceTest {
+
+    @Mock
+    private RoleRepository roleRepository;
+
+    @InjectMocks
+    private RoleServiceImpl roleService;
+
+    @Test
+    @Order(1)
+    @DisplayName(value = "Context loads")
+    void contextLoadsTest() {
+        Assertions.assertNotNull(roleRepository);
+        Assertions.assertNotNull(roleService);
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName(value = "Find Role by RoleName(RoelEnum)")
+    void mustReturnRoleByRoleEnum() {
+
+        final RoleEnum roleEnum = RoleEnum.ROLE_ADMIN;
+        final Role role = Role.builder()
+                .role(roleEnum)
+                .build();
+
+        Mockito.when(roleRepository.findByRole(roleEnum))
+                .thenReturn(Optional.of(role));
+
+        Role foundRole = roleService.findByRole(roleEnum);
+
+        Mockito.verify(roleRepository, Mockito.times(1)).findByRole(roleEnum);
+
+        Assertions.assertNotNull(foundRole);
+        Assertions.assertEquals(role, foundRole);
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName(value = "Find Role by RoleName(RoelEnum). Throw RoleIsNotFoundException.")
+    void mustThrowExceptionInFindByRoleEnum() {
+
+        final RoleEnum roleEnum = RoleEnum.ROLE_ADMIN;
+
+        Mockito.when(roleRepository.findByRole(roleEnum))
+                .thenReturn(Optional.empty());
+
+        Assertions.assertThrows(RoleIsNotFoundException.class, () ->
+                roleService.findByRole(roleEnum));
+
+        Mockito.verify(roleRepository, Mockito.times(1)).findByRole(roleEnum);
+    }
+}
