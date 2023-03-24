98f21af8e5d93c7cbb873e6bb02636bfac66307c
Lelikbjmak <denissolovey990@gmail.com>
2023-03-24 12:33:22 +0300

    feature: Add UserDetails implementation.

---
+++ src/main/java/com/innowise/employeedatasystem/security/ApplicationUserDetails.java
@@ -0,0 +1,57 @@
+package com.innowise.employeedatasystem.security;
+
+import com.innowise.employeedatasystem.entity.User;
+import lombok.Builder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Builder
+public class ApplicationUserDetails implements UserDetails {
+
+    private final User user;
+
+    public ApplicationUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRole().name()))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.user.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.user.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.user.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.user.isEnabled();
+    }
+}
