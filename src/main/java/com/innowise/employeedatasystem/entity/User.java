b3c089d607d700644557932bb130282cc850556c
Lelikbjmak <denissolovey990@gmail.com>
2023-03-24 12:32:52 +0300

    hotfix: Divide UserDetails and User Entity.

--- src/main/java/com/innowise/employeedatasystem/entity/User.java
+++ src/main/java/com/innowise/employeedatasystem/entity/User.java
@@ -4,22 +4,17 @@ package com.innowise.employeedatasystem.entity;
 import com.innowise.employeedatasystem.util.EntityConstant;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Entity
+@Getter
 @Setter
 @Builder
-@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = EntityConstant.Table.TABLE_USERS)
-public class User implements UserDetails {
+public class User {
 
     @Id
     @Getter
@@ -51,41 +46,4 @@ public class User implements UserDetails {
     private boolean accountNonLocked;
     private boolean credentialsNonExpired;
     private boolean enabled;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return roles.stream()
-                .map(role -> new SimpleGrantedAuthority(role.getRole().name()))
-                .collect(Collectors.toSet());
-    }
-
-    @Override
-    public String getPassword() {
-        return this.password;
-    }
-
-    @Override
-    public String getUsername() {
-        return this.username;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return this.accountNonExpired;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return this.accountNonLocked;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return this.credentialsNonExpired;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return enabled;
-    }
 }
