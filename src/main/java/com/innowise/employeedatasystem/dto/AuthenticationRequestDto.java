43c1a5fc20cab034234d26a048a4336f18dfafc1
Lelikbjmak <denissolovey990@gmail.com>
2023-03-28 23:53:41 +0300

    feature: Implement validation features for AuthenticationRequestDto.

--- src/main/java/com/innowise/employeedatasystem/dto/AuthenticationRequestDto.java
+++ src/main/java/com/innowise/employeedatasystem/dto/AuthenticationRequestDto.java
@@ -1,16 +1,22 @@
 package com.innowise.employeedatasystem.dto;
 
-import lombok.*;
+import com.innowise.employeedatasystem.util.EntityConstant;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public class AuthenticationRequestDto {
 
+    @NotBlank(message = EntityConstant.Validation.User.USERNAME_MANDATORY_CONSTRAINT_MASSAGE)
     private String username;
 
-    private String password;
 
+    @NotBlank(message = EntityConstant.Validation.User.PASSWORD_MANDATORY_CONSTRAINT_MASSAGE)
+    private String password;
 }
