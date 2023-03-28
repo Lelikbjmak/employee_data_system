b9d452180910145249251e96ac96231c557d7f73
Lelikbjmak <denissolovey990@gmail.com>
2023-03-28 23:56:28 +0300

    feature: Implement validation features for Controller level (using @Valid & @Validated for request body etc...).

--- src/main/java/com/innowise/employeedatasystem/controller/AuthenticationController.java
+++ src/main/java/com/innowise/employeedatasystem/controller/AuthenticationController.java
@@ -4,6 +4,7 @@ import com.innowise.employeedatasystem.dto.AuthenticationRequestDto;
 import com.innowise.employeedatasystem.dto.AuthenticationSuccessResponseDto;
 import com.innowise.employeedatasystem.service.AuthenticationService;
 import com.innowise.employeedatasystem.util.ApiConstant;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +18,7 @@ public class AuthenticationController {
 
     @PostMapping(value = ApiConstant.ApiPath.SIGN_IN)
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public AuthenticationSuccessResponseDto authenticateUser(@RequestBody AuthenticationRequestDto authenticationRequest) {
+    public AuthenticationSuccessResponseDto authenticateUser(@RequestBody @Valid AuthenticationRequestDto authenticationRequest) {
         return authenticationService.authenticateUser(authenticationRequest);
     }
 
