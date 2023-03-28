d35f01d6c410615c9eede34eae2769daaf624535
Lelikbjmak <denissolovey990@gmail.com>
2023-03-28 23:52:02 +0300

    feature: Add @Valid for nested RegistrationEmployeeDto & RegistrationUserDto.

--- src/main/java/com/innowise/employeedatasystem/dto/RegistrationDto.java
+++ src/main/java/com/innowise/employeedatasystem/dto/RegistrationDto.java
@@ -1,17 +1,21 @@
 package com.innowise.employeedatasystem.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.Valid;
+import lombok.*;
 
 @Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegistrationDto {
 
+    @Valid
+    @JsonProperty(value = "user")
     private RegistrationUserDto userDto;
 
+    @Valid
+    @JsonProperty(value = "employee")
     private RegistrationEmployeeDto employeeDto;
 }
