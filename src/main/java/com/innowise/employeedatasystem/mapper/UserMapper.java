8062d08ee3cecb8b5e791d7beadba517f9552e2c
Lelikbjmak <denissolovey990@gmail.com>
2023-03-29 22:19:22 +0300

    refactor: Replace UserMapper with MapStruct implementation.

--- src/main/java/com/innowise/employeedatasystem/mapper/UserMapper.java
+++ src/main/java/com/innowise/employeedatasystem/mapper/UserMapper.java
@@ -3,38 +3,16 @@ package com.innowise.employeedatasystem.mapper;
 import com.innowise.employeedatasystem.dto.RegistrationUserDto;
 import com.innowise.employeedatasystem.dto.UserDto;
 import com.innowise.employeedatasystem.entity.User;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 
-@Component
-@RequiredArgsConstructor
-public class UserMapper {
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
+        uses = RoleSetMapper.class)
+public interface UserMapper {
 
-    private final RoleSetMapper roleSetMapper;
+    User mapToEntity(UserDto userDto);
 
-    public User toUserEntity(RegistrationUserDto registrationUserDto) {
-        return User.builder()
-                .username(registrationUserDto.getUsername())
-                .password(registrationUserDto.getPassword())
-                .roles(roleSetMapper.toRoleEntitySet(registrationUserDto.getRoles()))
-                .mail(registrationUserDto.getMail())
-                .enabled(true)
-                .accountNonLocked(true)
-                .accountNonExpired(true)
-                .credentialsNonExpired(true)
-                .build();
-    }
+    User mapToEntity(RegistrationUserDto userDto);
 
-    public UserDto toUserDto(User user) {
-        return UserDto.builder()
-                .username(user.getUsername())
-                .mail(user.getMail())
-                .roles(roleSetMapper.toRoleDtoSet(user.getRoles()))
-                .enabled(user.isEnabled())
-                .accountNonExpired(user.isAccountNonExpired())
-                .accountNonLocked(user.isAccountNonLocked())
-                .credentialsNonExpired(user.isCredentialsNonExpired())
-                .build();
-    }
-
+    UserDto mapToDto(User user);
 }
