9d38dc4c3eeea19154c7b66bfa258deb4cc33926
Lelikbjmak <denissolovey990@gmail.com>
2023-03-29 22:19:39 +0300

    refactor: Replace EmployeeMapper with MapStruct implementation.

--- src/main/java/com/innowise/employeedatasystem/mapper/EmployeeMapper.java
+++ src/main/java/com/innowise/employeedatasystem/mapper/EmployeeMapper.java
@@ -5,80 +5,25 @@ import com.innowise.employeedatasystem.dto.EmployeeDto;
 import com.innowise.employeedatasystem.dto.RegistrationEmployeeDto;
 import com.innowise.employeedatasystem.dto.UpdatedEmployeeDto;
 import com.innowise.employeedatasystem.entity.Employee;
-import com.innowise.employeedatasystem.serviceimpl.EmployeeServiceImpl;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
 
-@Component
-@RequiredArgsConstructor
-public class EmployeeMapper {
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
+        uses = UserMapper.class)
+public interface EmployeeMapper {
 
-    private static final Boolean IS_EMPLOYEE_DELETED = true;
+    @Mapping(source = "userDto", target = "user")
+    Employee mapToEntity(EmployeeDto employeeDto);
 
-    private static final Boolean IS_EMPLOYEE_UPDATED = true;
+    Employee mapToEntity(RegistrationEmployeeDto employeeDto);
 
-    private final EmployeeServiceImpl employeeService;
+    @Mapping(source = "user", target = "userDto")
+    EmployeeDto mapToDto(Employee employee);
 
-    private final UserMapper userMapper;
+    @Mapping(target = "isUpdated", expression = "java(true)")
+    UpdatedEmployeeDto mapToUpdatedEmployeeDto(Employee employee);
 
-    public Employee toEmployeeEntity(RegistrationEmployeeDto registrationEmployeeDto) {
-        return Employee.builder()
-                .firstName(registrationEmployeeDto.getFirstName())
-                .middleName(registrationEmployeeDto.getMiddleName())
-                .lastName(registrationEmployeeDto.getLastName())
-                .hireDate(registrationEmployeeDto.getHireDate())
-                .build();
-    }
-
-    public EmployeeDto toEmployeeDto(Employee employeeEntity) {
-        return EmployeeDto.builder()
-                .id(employeeEntity.getId() != null ? employeeEntity.getId() : null)
-                .firstName(employeeEntity.getFirstName())
-                .lastName(employeeEntity.getLastName())
-                .middleName(employeeEntity.getMiddleName())
-                .hireDate(employeeEntity.getHireDate())
-                .userDto(userMapper.toUserDto(employeeEntity.getUser()))
-                .build();
-    }
-
-    public DeletedEmployeeDto toDeletedEmployeeDto(Employee employeeEntity) {
-        return DeletedEmployeeDto.builder()
-                .id(employeeEntity.getId() != null ? employeeEntity.getId() : null)
-                .firstName(employeeEntity.getFirstName())
-                .lastName(employeeEntity.getLastName())
-                .middleName(employeeEntity.getMiddleName())
-                .hireDate(employeeEntity.getHireDate())
-                .isDeleted(IS_EMPLOYEE_DELETED)
-                .build();
-    }
-
-    public Employee toEmployeeEntityFromDatabase(EmployeeDto employeeDto) {
-
-        boolean isIdPresent = employeeDto.getId() != null;
-        Employee employee;
-
-        if (isIdPresent) {
-            employee = employeeService.getEmployeeById(employeeDto.getId());
-
-        } else if (employeeDto.getUserDto() == null) {
-            employee = employeeService.findByFirstLastMiddleNameAndHireDate(employeeDto.getFirstName(), employeeDto.getLastName(),
-                    employeeDto.getMiddleName(), employeeDto.getHireDate());
-
-        } else {
-            employee = employeeService.findEmployeeByUserUsername(employeeDto.getUserDto().getUsername());
-        }
-
-        return employee;
-    }
-
-    public UpdatedEmployeeDto toUpdatedEmployeeDto(Employee employee) {
-        return UpdatedEmployeeDto.builder()
-                .id(employee.getId() != null ? employee.getId() : null)
-                .firstName(employee.getFirstName())
-                .lastName(employee.getLastName())
-                .middleName(employee.getMiddleName())
-                .hireDate(employee.getHireDate())
-                .isUpdated(IS_EMPLOYEE_UPDATED)
-                .build();
-    }
+    @Mapping(target = "isDeleted", expression = "java(true)")
+    DeletedEmployeeDto mapToDeletedEmployeeDto(Employee employee);
 }
