c2d25bce8cd1a279fb1d065f5e5216d8e91bdfa6
Lelikbjmak <denissolovey990@gmail.com>
2023-03-29 22:22:36 +0300

    feature: Add RoleMapper MapStruct implementation for single Role entity.

---
+++ src/main/java/com/innowise/employeedatasystem/mapper/RoleMapper.java
@@ -0,0 +1,35 @@
+package com.innowise.employeedatasystem.mapper;
+
+import com.innowise.employeedatasystem.entity.Role;
+import com.innowise.employeedatasystem.entity.RoleEnum;
+import com.innowise.employeedatasystem.exception.RoleIsNotFoundException;
+import com.innowise.employeedatasystem.repo.RoleRepository;
+import com.innowise.employeedatasystem.util.GeneralConstant;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.Instant;
+import java.util.Map;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public abstract class RoleMapper {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    public String mapToDto(Role role) {
+        return role.getRole().name();
+    }
+
+    public Role mapToEntity(String stringRole) {
+        try {
+            RoleEnum roleEnum = RoleEnum.valueOf(stringRole);
+            return roleRepository.findByRole(roleEnum).orElseThrow(() -> new RoleIsNotFoundException(GeneralConstant.Message.ROLE_IS_NOT_FOUND_EXCEPTION_MESSAGE,
+                    Instant.now(), Map.of(GeneralConstant.Field.ROLE_FIELD, stringRole)));
+        } catch (IllegalArgumentException exception) {
+            throw new RoleIsNotFoundException(GeneralConstant.Message.ROLE_IS_NOT_FOUND_EXCEPTION_MESSAGE,
+                    Instant.now(), Map.of(GeneralConstant.Field.ROLE_FIELD, stringRole));
+        }
+    }
+}
