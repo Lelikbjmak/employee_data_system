e5d284f9f653d1aa9c3017f8b48ab1485014c760
Lelikbjmak <denissolovey990@gmail.com>
2023-03-29 22:24:51 +0300

    feature: Add RoleMapperTest for RoleMapper.

---
+++ src/test/java/com/innowise/employeedatasystem/mapper/RoleMapperTest.java
@@ -0,0 +1,56 @@
+package com.innowise.employeedatasystem.mapper;
+
+import com.innowise.employeedatasystem.entity.Role;
+import com.innowise.employeedatasystem.entity.RoleEnum;
+import com.innowise.employeedatasystem.exception.RoleIsNotFoundException;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class RoleMapperTest {
+
+    @Autowired
+    private RoleMapper roleMapper;
+
+    @Test
+    @Order(1)
+    @DisplayName(value = "Context loads")
+    void contextLoads() {
+        Assertions.assertNotNull(roleMapper);
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName(value = "Map String to Role.")
+    void mustMapStringToEntity() {
+
+        Role role = roleMapper.mapToEntity("ROLE_USER");
+        Assertions.assertNotNull(role);
+        Assertions.assertEquals(2, role.getId());
+        Assertions.assertEquals(RoleEnum.ROLE_USER, role.getRole());
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName(value = "Map Role to String.")
+    void mustMapRoleToString() {
+
+        String role = roleMapper.mapToDto(Role.builder()
+                .id(1L)
+                .role(RoleEnum.ROLE_ADMIN)
+                .build());
+
+        Assertions.assertNotNull(role);
+        Assertions.assertEquals(RoleEnum.ROLE_ADMIN.name(), role);
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName(value = "Map String to Role (FAILED ROLE NOT EXISTS).")
+    void mustThrowRoleNotFoundException() {
+        Assertions.assertThrows(RoleIsNotFoundException.class,
+                () -> roleMapper.mapToEntity("INVALID"));
+    }
+}
