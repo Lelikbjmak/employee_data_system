728e609e5d454c3163a9fa16a85dcafb96a44c01
Lelikbjmak <denissolovey990@gmail.com>
2023-03-31 19:04:56 +0300

    refactor: refactor apiRoutes for tests.

--- src/test/java/com/innowise/employeedatasystem/util/Constant.java
+++ src/test/java/com/innowise/employeedatasystem/util/Constant.java
@@ -8,11 +8,12 @@ public class Constant {
 
     public static class ApiRoutes {
         public static final String AUTH_ROUTE = "/api/auth/signIn";
-        public static final String GET_X = "/api/employee/get";
+        public static final String GET_X_BY_ID = "/api/employee/get/id/{id}";
+        public static final String GET_X_BY_USERNAME = "/api/employee/get/username/{username}";
         public static final String GET_ALL_X = "/api/employee/all";
-        public static final String DELETE_X = "/api/employee/delete";
+        public static final String DELETE_X = "/api/employee/delete/id/{id}";
         public static final String DELETE_ALL_X = "/api/employee/delete/all";
-        public static final String EDIT_X = "/api/employee/edit";
+        public static final String EDIT_X = "/api/employee/edit/id/{id}";
         public static final String EDIT_ALL_X = "/api/employee/edit/all";
         public static final String ADD_X = "/api/employee/add";
         public static final String ADD_ALL_X = "/api/employee/add/all";
