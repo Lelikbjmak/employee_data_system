2b314f4af660aa1f61c92bba2457ddc773034fe7
Lelikbjmak <denissolovey990@gmail.com>
2023-04-02 14:09:22 +0300

    feature: Create MapStructConstant to implement constants fort mapping (expression = ...).

---
+++ src/main/java/com/innowise/employeedatasystem/util/MapStructConstant.java
@@ -0,0 +1,8 @@
+package com.innowise.employeedatasystem.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class MapStructConstant {
+    public final String SET_IS_UPDATED_OR_DELETED_FIELD_EXPRESSION = "java(true)";
+}
